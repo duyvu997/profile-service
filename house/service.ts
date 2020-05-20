d3c71446281a66d77fe62cbc4f3b181e42f49e47
duy-vu <ngocduy0997@gmail.com>
2020-05-20 10:13:59 +0700

    Revert "Feature/Add filter houses by location"

--- house/service.ts
+++
@@ -1,8 +0,0 @@
-import { houseRepository } from "./repo";
-
-export class HouseService {
-  public async getHouses(offset: number, limit: number, filter: any) {
-    return houseRepository.getHouses(offset, limit, filter);
-  }
-}
-export const houseService = new HouseService();
