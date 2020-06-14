0aea537c6b6f1398b466a63c0a80d1384f5213b1
duyvu997 <ngocduy0997@gmail.com>
2020-06-14 14:02:47 +0700

    Revert "Revert "Feature/Add filter houses by location""
    This reverts commit d3c71446281a66d77fe62cbc4f3b181e42f49e47.

---
+++ common/custom.validator.ts
@@ -0,0 +1,22 @@
+// tslint:disable: max-classes-per-file
+import {
+  ValidationArguments,
+  Validator,
+  ValidatorConstraint,
+  ValidatorConstraintInterface
+} from "class-validator";
+
+@ValidatorConstraint({ name: "isIntergerNumber", async: false })
+export class IsIntergerNumber implements ValidatorConstraintInterface {
+  public validate(propertyValue: string, args: ValidationArguments) {
+    return Number.isInteger(Number(propertyValue));
+  }
+}
+
+@ValidatorConstraint({ name: "isPositiveNumberString", async: false })
+export class IsPositiveNumberString implements ValidatorConstraintInterface {
+  public validate(propertyValue: string, args: ValidationArguments) {
+    const validator = new Validator();
+    return validator.isPositive(Number(propertyValue));
+  }
+}
